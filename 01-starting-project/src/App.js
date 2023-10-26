023f8c572868f5ff7257aa798756282fb07a59a0
TomaszKoniecznyIT <tkonieczny.it@wp.pl>
2023-10-26 12:10:07 +0100

    useing dynamic routes

--- 01-starting-project/src/App.js
+++ 01-starting-project/src/App.js
@@ -8,6 +8,7 @@ import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -29,6 +30,7 @@ const router = createBrowserRouter([
         path: "/products",
         element: <ProductsPage />,
       },
+      { path: "/products/:productId", element: <ProductDetailPage /> },
     ],
   },
 ]);
